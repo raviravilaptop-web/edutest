2c9a495b67f45681e693099f2fd66ab74c3c78a9
raviravilaptop-web <raviravilaptop@gmail.com>
2025-12-19 01:01:36 +0530

    Update about.php

--- about.php
+++ about.php
@@ -1,11 +1,12 @@
+<!DOCTYPE html>
 <html>
-    <head>
-            <title>OpenShift Test - About</title>
+<head>
+    <title>About About About</title>
     <link rel="stylesheet" href="style.css">
-    </head>
+</head>
 <body>
     <div class="glass-card">
-    <h1>About This Project</h1>
+    <h1>About This Projec.............t</h1>
     <p>This project is designed to test the S2I (Source-to-Image) capabilities of Red Hat OpenShift.</p>
     <div class="btn-container">
         <a href="index.php" class="btn">Back to Home</a>
@@ -14,3 +15,4 @@
 </div>
 </body>
 </html>
+
